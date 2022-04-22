f5ab68babec8bc4989033a1695d810b1ba5ba2cd
mveres <veres85@yahoo.com>
2022-04-22 12:58:45 +0300

    restored css modules for transitions showcase

--- css-showcases/src/transition-helper.js
+++ css-showcases/src/transition-helper.js
@@ -1,54 +1,19 @@
-const styles = /*css*/ `.transition-box-label {
-  font-family: monospace;
-  margin: 0.125rem;
-  font-size: 0.75rem;
-  color: #fff;
-}
-
-.transition-box-label-sm {
-  font-size: 0.55rem;
-}
-
-.transition-box-wrapper {
-  display: flex;
-  flex-direction: column;
-}
-
-.transition-box {
-  cursor: pointer;
-  height: 6rem;
-  width: 12rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #fff;
-  border-radius: 0.375rem;
-  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
-  background-color: #6366f1;
-}
-`;
+import styles from './transition-box.module.scss';
 
 export const getTransitionsHtml = (element, props, mode) => {
-  function style(name) {
-    let rules = [];
+  const style = (name) => {
     if (mode === 'animation') {
-      rules.push(`animation: var(${name}) forwards`);
+      return `animation: var(${name}) forwards`;
     } else {
-      rules.push('transition-delay: 0');
-      rules.push(
-        `transition-duration: ${mode === 'time' ? `var(${name})` : '1s'}`
-      );
-      rules.push('transition-property: margin-left');
-      rules.push(
-        `transition-timing-function: ${
+      return `
+        transition-delay: 0;
+        transition-duration: ${mode === 'time' ? `var(${name})` : '1s'};
+        transition-property: margin-left';
+        transition-timing-function: ${
           mode === 'ease' ? `var(${name})` : 'linear'
-        }`
-      );
+        };`;
     }
-
-    return rules.join(';');
-  }
+  };
 
   setTimeout(() => {
     const boxes = element.querySelectorAll('.transitionBox');
@@ -62,24 +27,25 @@ export const getTransitionsHtml = (element, props, mode) => {
     );
   }, 300);
 
+  const getAnimationMode = () => (mode === 'animation' ? 'no-anim' : '');
+
+  const boxWidth = '12rem';
+
   return /*html*/ `
-  <style>${styles}</style>
-<div class="transition-box-wrapper">
+<div id="transitionBoxesContainer" class="${styles.boxesContainer}">
   <style>
-    .clicked { margin-left: 20rem; }
+    .clicked { margin-left: calc(100% - 12rem); }
     .no-anim { animation: none !important; }
   </style>
   ${props
     .map(
       ([name, value], i) => /*html*/ `
       <div id="transitionBox${i}"
-          class="transition-box ${
-            mode === 'animation' ? 'no-anim' : ''
-          } transitionBox"
-          style="${style(name)}"
+          class="${styles.box} ${getAnimationMode()} transitionBox"
+          style="width: ${boxWidth} ${style(name)}"
       >
-        <div class="transition-box-label">${name}</div>
-        <div class="transition-box-label transition-box-label-sm">${value}</div>
+        <div class="${styles.boxLabel}">${name}</div>
+        <div class="${styles.boxLabel} ${styles.boxLabelSmall}">${value}</div>
       </div>`
     )
     .join('\n')}
