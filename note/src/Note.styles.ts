03be5847a384f3cce82135a71808e7e0ea36cab6
mveres <veres85@yahoo.com>
2022-04-28 11:35:44 +0300

    fixed note color css prop

--- note/src/Note.styles.ts
+++ note/src/Note.styles.ts
@@ -1,9 +1,9 @@
-import { css, CSSResult } from 'lit';
+import { css } from 'lit';
 
 export const colors = {
-  info: '#54AEFF66',
-  warning: '#ECE81A66',
-  error: '#FF818266',
+  info: css`#54AEFF66`,
+  warning: css`#ECE81A66`,
+  error: css`#FF818266`,
 };
 
 export const NoteStyles = css`
@@ -12,7 +12,7 @@ export const NoteStyles = css`
     padding: 1rem;
     margin-bottom: 1rem;
     border-radius: 0.25rem;
-    border-left: 0.25rem solid var(--dockit-note-color);
-    background-color: var(--dockit-note-color);
+    border-left: 0.25rem solid var(--dockit-note-color, ${colors.info});
+    background-color: var(--dockit-note-color, ${colors.info});
   }
 `;
